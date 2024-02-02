6ecdec01ea033aca157b11240fa2c2b18ff3cbc7
Chanel Morgan <chanelmorgan05@icloud.com>
2024-02-02 13:51:03 +0000

    Creating the socket

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,16 @@
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 
 public class Server {
@@ -8,13 +18,13 @@ public class Server {
     // global static variable to hold the files the user wants to send over
     static ArrayList<MyFile> myFiles = new ArrayList<>();
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException {
 
         int fileId = 0;
 
         // Creating the gui
         JFrame jFrame = new JFrame("Server");
-        jFrame.setSize(400, 400 );
+        jFrame.setSize(400, 400);
         jFrame.setLayout(new BoxLayout(jFrame.getContentPane(), BoxLayout.Y_AXIS));
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -38,7 +48,196 @@ public class Server {
 
         // Creating the server socket
 
+        // A server socket waits for requests to come in over the network, performs some operation based on that request
+        // and can return a result to the requester
+        ServerSocket serverSocket = new ServerSocket(1234);
+
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+
+                int fileNameLength = dataInputStream.readInt();
+
+                if (fileNameLength > 0) {
+                    byte[] fileNameBytes = new byte[fileNameLength];
+                    dataInputStream.readFully(fileNameBytes, 0, fileNameBytes.length);
+                    String fileName = new String(fileNameBytes);
+
+                    int fileContentLength = dataInputStream.readInt();
+
+                    if (fileContentLength > 0) {
+                        byte[] fileContentBytes = new byte[fileContentLength];
+                        dataInputStream.readFully(fileContentBytes, 0, fileContentLength);
+
+                        // Creating the rows to hold the files
+                        JPanel jpFileRow = new JPanel();
+                        jpFileRow.setLayout(new BoxLayout(jpFileRow, BoxLayout.Y_AXIS));
+
+                        // Creating the titles for the file names
+                        JLabel jlFileName = new JLabel(fileName);
+                        jlFileName.setFont(new Font("Arial", Font.BOLD, 20));
+                        jlFileName.setBorder(new EmptyBorder(10, 0, 10, 0));
+
+                        if (getFileExtension(fileName).equalsIgnoreCase("txt")){
+                            jpFileRow.setName(String.valueOf(fileId));
+                            jpFileRow.addMouseListener(getMyMouseListener());
+
+                            jpFileRow.add(jlFileName);
+                            jPanel.add(jpFileRow);
+                            jFrame.validate();
+                        } else {
+                            jpFileRow.setName(String.valueOf(fileId));
+                            jpFileRow.addMouseListener(getMyMouseListener());
+                            jpFileRow.add(jlFileName);
+                            jPanel.add(jpFileRow);
+
+                            jFrame.validate();
+                        }
+                            myFiles.add(new MyFile(fileId, fileName, fileContentBytes, getFileExtension(fileName)));
+                    }
+
+                }
+
+            } catch (IOException error) {
+                error.printStackTrace();
+
+            }
+        }
 
 
+    }
+
+    public static MouseListener getMyMouseListener(){
+
+        return new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                JPanel jPanel = (JPanel) e.getSource();
+                int fieldId = Integer.parseInt(jPanel.getName());
+
+                for (MyFile myFile : myFiles) {
+                    JFrame jfPreview = createFrame(myFile.getName(), myFile.getData(), myFile.getFileExtension());
+                    jfPreview.setVisible(true);
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        };
+    }
+
+    public static JFrame createFrame(String fileName, byte[] fileData, String fileExtension) {
+        // Creating the frame
+        JFrame jFrame = new JFrame("File downloader");
+        jFrame.setSize(400, 400);
+
+        // Creating the panel
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
+
+        // Creating the label
+        JLabel jlTitle = new JLabel("File downloader");
+        jlTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+        jlTitle.setFont(new Font("Arial", Font.BOLD, 25));
+        jlTitle.setBorder(new EmptyBorder(20, 0, 10, 0));
+
+        // Creating the popup
+        JLabel jlPrompt = new JLabel("Are you sure you want to download " + fileName);
+        jlPrompt.setAlignmentX(Component.CENTER_ALIGNMENT);
+        jlPrompt.setFont(new Font("Arial", Font.BOLD, 20));
+        jlPrompt.setBorder(new EmptyBorder(20, 0, 10, 0));
+
+        // Creating the buttons
+        JButton jbYes = new JButton("Yes");
+        jbYes.setPreferredSize(new Dimension(150, 75));
+        jbYes.setFont(new Font("Arial", Font.BOLD, 20));
+
+        JButton jbNo = new JButton("No");
+        jbNo.setPreferredSize(new Dimension(150, 75));
+        jbNo.setFont(new Font("Arial", Font.BOLD, 20));
+
+        // Creating the labels to display the content
+        JLabel jlFileContent = new JLabel();
+        jlFileContent.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JPanel jpButtons = new JPanel();
+        jpButtons.setBorder(new EmptyBorder(20, 0, 10,0));
+        jpButtons.add(jbYes);
+        jpButtons.add(jbNo);
+
+        if(fileExtension.equalsIgnoreCase("txt")){
+            // allows the content to wrap
+            jlFileContent.setText("<html>" + new String(fileData) + "</html>");
+
+        } else {
+            jlFileContent.setIcon(new ImageIcon(fileData));
+        }
+
+        // adding the action listener to the yes button
+        jbYes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                File fileToDownload = new File(fileName);
+
+                try{
+                    FileOutputStream fileOutputStream = new FileOutputStream(fileToDownload);
+
+                    fileOutputStream.write(fileData);
+                    fileOutputStream.close();
+                    jFrame.dispose();
+                }catch(IOException error){
+                    error.printStackTrace();
+
+                }
+            }
+        });
+
+        // Adding the action listener to the no button
+        jbNo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jFrame.dispose();
+            }
+        });
+
+        // Adding all the components to the panels
+        jPanel.add(jlTitle);
+        jPanel.add(jlPrompt);
+        jPanel.add(jlFileContent);
+        jPanel.add(jpButtons);
+
+        jFrame.add(jPanel);
+        return jFrame;
+
+
+    }
+
+    // Method is static because we are using it in the static method which is main
+    public static String getFileExtension(String fileName) {
+        int i = fileName.lastIndexOf('.');
+
+        if (i > 0) {
+            return fileName.substring(i + 1);
+        } else {
+            return "No extension found";
+        }
     }
 }
